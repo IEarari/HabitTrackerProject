e36b01a3acdefb4ffc861e3cac0c27ffc38a196d
Shinichi02 <aarar90@gmail.com>
2017-12-27 21:20:57 +0200

    Update MainActivity.java

--- app/src/main/java/com/ibraheem/android/habittrackerproject/MainActivity.java
+++ app/src/main/java/com/ibraheem/android/habittrackerproject/MainActivity.java
@@ -22,19 +22,17 @@ public class MainActivity extends AppCompatActivity {
         String dateString = formatter.format(date);
         int dateInt = Integer.parseInt(dateString);
 
-            habitDbHelper.addHabit(dateInt, HabitContract.HabitEntry.HABIT_1,
-                    "Go Running everyday");
-            habitDbHelper.addHabit(dateInt, HabitContract.HabitEntry.HABIT_2,
-                    "study for school everyday");
-            habitDbHelper.addHabit(dateInt, HabitContract.HabitEntry.HABIT_3,
-                    "learn From udacity");
-            habitDbHelper.addHabit(dateInt, HabitContract.HabitEntry.HABIT_4,
-                    "Read A Book once in 2 weeks");
+        habitDbHelper.addHabit(dateInt, HabitContract.HabitEntry.HABIT_1,
+                "Go Running everyday");
+        habitDbHelper.addHabit(dateInt, HabitContract.HabitEntry.HABIT_2,
+                "study for school everyday");
+        habitDbHelper.addHabit(dateInt, HabitContract.HabitEntry.HABIT_3,
+                "learn From udacity");
+        habitDbHelper.addHabit(dateInt, HabitContract.HabitEntry.HABIT_4,
+                "Read A Book once in 2 weeks");
         Cursor cursor = habitDbHelper.readHabits();
         while (cursor.moveToNext()) {
             Log.v(TAG, "habit: " + cursor.getInt(0) + " " + cursor.getInt(1) +
-                    " " + cursor.getInt(2) + " " + cursor.getString(3));
-            Log.e(TAG, "habit: " + cursor.getInt(0) + " " + cursor.getInt(1) +
                     " " + cursor.getInt(2) + " " + cursor.getString(3));
         }
     }
